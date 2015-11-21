619e64614c35337f5143d07b81fc6a14555d5a1e
Leandro <tecnico.kaio@gmail.com>
2015-11-21 03:48:20 -0200

    initial commit

---
+++ configs/database.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Admin on 11/21/15.
+ */
+
+
+module.exports = {
+    'url': ''
+};
---
+++ server.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Admin on 11/21/15.
+ */
+
+
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var configDB = require('./configs/database.js');
+
+mongoose.connect(configDB.url);
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+
+app.use(session({secret: 'ilovemywifebrunaevangelista'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./app/routes.js')(app, passport);
+
+app.listen(port);
+console.log('Running server on port: %s', port);
